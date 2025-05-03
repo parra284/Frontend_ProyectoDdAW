d6e53cfc18c7271d98b9598823f6ace4912cb493
Alejandro Parra <alejandropaga@unisabana.edu.co>
2025-05-03 18:25:02 -0500

    Create frontend repo

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import ReactDOM from 'react-dom/client';
+import React from 'react';
+
+function App() {
+  return (<h1>Hola</h1>);
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <div>
+    <App />
+  </div>,
+)
