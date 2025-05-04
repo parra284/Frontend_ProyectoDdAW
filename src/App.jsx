ff20ad7fe87a1e6d5e0b7f79908ff4b8a871a4ec
Alejandro Parra <alejandropaga@unisabana.edu.co>
2025-05-03 21:12:02 -0500

    Set tailwind and basic routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import ReactDOM from 'react-dom/client';
 import React from 'react';
+import './index.css';
 
 function App() {
   return (<h1>Hola</h1>);
 }
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <div>
+  <React.StrictMode>
     <App />
-  </div>,
-)
+  </React.StrictMode>
+);
