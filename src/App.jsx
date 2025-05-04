cdddfddbe4c382d053d2f93e2604cfba3e541db1
Alejandro Parra <alejandropaga@unisabana.edu.co>
2025-05-04 07:09:15 -0500

    Set react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
 import ReactDOM from 'react-dom/client';
 import React from 'react';
 import './index.css';
-
-function App() {
-  return (<h1>Hola</h1>);
-}
+import Router from './router';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Router />
   </React.StrictMode>
 );
--- src/router.jsx
+++ src/router.jsx
@@ -1,1 +1,11 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, RouterProvider, Navigate } from "react-router-dom";
+import SignUp from './auth/SignUp';
+
+const router = createBrowserRouter([
+    {path: "/", element: <Navigate to="/signup" replace />},
+    {path: "/signup", element: <SignUp />}
+]);
+
+export default function Router() {
+    return <RouterProvider router={router} />;
+}
